eef65e6974966a0514d24efc18dde81af60cb47f
nicolasbuitrago <nicolasbuitrago@uninorte.edu.co>
2017-10-27 18:10:56 -0500

    Lista animacion de Pacman e implementado a la perfeccion el ejemplo

--- src/pacman/Pacman.java
+++ src/pacman/Pacman.java
@@ -5,17 +5,78 @@
  */
 package pacman;
 
+import java.awt.Graphics;
+import javax.swing.ImageIcon;
+
 /**
  *
- * @author Nicolás
+ * @author nicolasbuitrago
  */
 public class Pacman {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+   public static final int UP=0;
+    public static final int RIGTH=1;
+    public static final int DOWN=2;
+    public static final int LEFT=3;
+    public static final int NONE=-1;
+    
+    Animation[] animations;
+    int x;
+    int y;
+    int vx;
+    int vy;
+    String path;
+    int currentAnimation;
+    int currentDirection;
+    
+    public Pacman(int x, int y, int vx, int vy, String path){
+        this.path=path;
+        this.x=x;
+        this.y=y;
+        this.vx=vx;
+        this.vy=vy;
+        this.currentDirection=-1;
+        animations=new Animation[4];
     }
+    
+    public void loadPics(String[] names)throws Exception{
+        for (int j=0;j<4;j++) {
+            String name=names[j];
+            animations[j]=new Animation();
+            for (int i = 1; i <= 2; i++) {
+                System.out.println("/" +path+"//"+name+i+".png");
+                animations[j].addScene(
+                new ImageIcon(getClass().getResource("/" +path+"//"+name+i+".png")).getImage()    , 100);
+            }
+        }
+       
+    }
+    public void moveRigth(long time){
+        x+=vx;
+        currentAnimation=Pacman.RIGTH;
+        animations[Pacman.RIGTH].update(time);
+    }
+    
+    public void moveLeft(long time){
+        x-=vx;
+        currentAnimation=Pacman.LEFT;
+        animations[Pacman.LEFT].update(time);
+    }
+    
+     public void moveUp(long time){
+        y-=vy;
+        currentAnimation=Pacman.UP;
+        animations[Pacman.UP].update(time);
+    }
+     
+     public void moveDown(long time){
+        y+=vy;
+        currentAnimation=Pacman.DOWN;
+        animations[Pacman.DOWN].update(time);
+    }
+     
+     public void draw(Graphics g){
+         g.drawImage(animations[currentAnimation].getImage(),x,y,null);
+     }
     
 }
---
+++ src/pacman/Principal.java
@@ -0,0 +1,118 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pacman;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author nicolasbuitrago
+ */
+public class Principal extends JFrame {
+
+    public Thread movieLoop;
+    public Canvas c;
+    public Pacman J1;
+    public int[][] mundo = {{1,0,0,1,0,0,0,1},
+                            {1,1,0,0,0,1,1,1},
+                            {1,0,0,0,0,0,0,1},
+                            {1,0,0,1,1,1,1,1},
+                            {1,0,0,0,0,0,0,1},
+                            {1,1,1,1,0,1,1,1}
+    };
+    public Principal(int w, int h)throws Exception{
+        c= new Canvas();
+        this.setSize(w, h);
+        c.setSize(w, h);
+        this.add(c);
+        this.addKeyListener(new KeyListener() {
+
+            @Override
+            public void keyTyped(KeyEvent e) {
+              
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+               switch(e.getKeyCode()){
+                   case KeyEvent.VK_UP:{J1.currentDirection=Pacman.UP; break;}
+                   case KeyEvent.VK_DOWN:{J1.currentDirection=Pacman.DOWN; break;}
+                   case KeyEvent.VK_LEFT:{J1.currentDirection=Pacman.LEFT; break;}
+                   case KeyEvent.VK_RIGHT:{J1.currentDirection=Pacman.RIGTH; break;}
+               }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+               switch(e.getKeyCode()){
+                   case KeyEvent.VK_UP:{J1.currentDirection=Pacman.NONE; break;}
+                   case KeyEvent.VK_DOWN:{J1.currentDirection=Pacman.NONE; break;}
+                   case KeyEvent.VK_LEFT:{J1.currentDirection=Pacman.NONE; break;}
+                   case KeyEvent.VK_RIGHT:{J1.currentDirection=Pacman.NONE; break;}
+               }
+            }
+        });
+        J1=new Pacman(50, 50, 8, 8, "PacmanSprites");//Los ultimos dos son velocidad
+        String []names={"arriba","adelante","abajo","atras"};
+        J1.loadPics(names);
+        movieLoop=new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                c.createBufferStrategy(2);
+                Graphics g=c.getBufferStrategy().getDrawGraphics();
+                long startTime=System.currentTimeMillis();
+                long currentTime=0;
+                while(true){
+                    try{
+//                        g.setColor(Color.BLACK);
+//                        g.fillRect(0,0, c.getWidth(), c.getHeight());                        
+                        for (int i = 0; i < 6; i++) {
+                            for (int j = 0; j < 8; j++) {
+                                if(mundo[i][j]==1){
+                                    g.setColor(Color.BLUE);
+                                    g.fillRect(100*j,100*i, 100, 100);
+                                }else{
+                                    g.setColor(Color.BLACK);
+                                    g.fillRect(100*j,100*i, 100, 100);
+                                }
+                            }
+                        }
+                        currentTime=System.currentTimeMillis()-startTime;
+                        switch(J1.currentDirection){
+                            case Pacman.RIGTH:{ J1.moveRigth(currentTime); break;}
+                            case Pacman.DOWN:{J1.moveDown(currentTime); break;}
+                            case Pacman.LEFT:{ J1.moveLeft(currentTime); break;}
+                            case Pacman.UP:{J1.moveUp(currentTime); break;}
+                        }
+                        J1.draw(g);
+                        Thread.sleep(30);
+                        c.getBufferStrategy().show();
+                    }catch(Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+    
+    public static void main(String[] args) {
+        try{
+            Principal p= new Principal(800,600);
+            p.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            p.setVisible(true);
+            p.movieLoop.start();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+}
